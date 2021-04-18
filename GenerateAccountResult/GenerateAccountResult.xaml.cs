419956d09cfbdb92d0b329f973a2e7ccfcad69d2
alicja.mruk <alicja.mruk@student.put.poznan.pl>
2021-04-18 14:22:58 +0200

    Add GenerateAccount Layout and Generate Account result layouts

---
+++ GenerateAccountResult/GenerateAccountResult.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LocalDatabase_Server.GenerateAccountResult
+{
+    /// <summary>
+    /// Interaction logic for GenerateAccountResult.xaml
+    /// </summary>
+    public partial class GenerateAccountResult : Window
+    {
+        public GenerateAccountResult()
+        {
+            InitializeComponent();
+        }
+    }
+}
