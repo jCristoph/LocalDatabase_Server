d9478d53f0afa8f02f316dd35a7b5586f2107e69
alicja.mruk <alicja.mruk@student.put.poznan.pl>
2021-04-19 14:05:34 +0200

    Add login panel and latest transacitions panel

---
+++ LoginPanel/LoginPanel.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LocalDatabase_Server.LoginPanel
+{
+    /// <summary>
+    /// Interaction logic for LoginPanel.xaml
+    /// </summary>
+    public partial class LoginPanel : Window
+    {
+        public LoginPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
